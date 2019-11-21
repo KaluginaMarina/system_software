a3a0141940913f1ca972b22fc0a6b16b01c6667b
Marina Kalugina <marin.kalugin@gmail.com>
2019-11-21 21:14:09 +0300

    [osp lab5] 6 подзадание

--- osp/lab5/part_2/threads.c
+++ osp/lab5/part_2/threads.c
@@ -18,6 +18,7 @@ void print_array() {
     printf("\n");
 }
 
+unsigned long time_threads = 1000000;
 void parse_flag(int argc, char *argv[]) {
     unsigned int opt = 0;
     if (argc == 1) {
@@ -45,20 +46,18 @@ void parse_flag(int argc, char *argv[]) {
                 break;
             case '3':
                 printf("Третье подзадание.\n");
-                time_t time;
                 if (argc == optind) {
-                    time = 1000000;
-                    printf("Не указано количество микросекунд.\nЗадано дефолтное значение TIME = %ld\n", time);
+                    printf("Не указано количество микросекунд.\nЗадано дефолтное значение TIME = %ld\n", time_threads);
                 } else {
                     char *p;
                     errno = 0;
-                    time = (int) strtol(argv[optind], &p, 10);
+                    time_threads = (unsigned long) strtol(argv[optind], &p, 10);
                     if (errno || *p != '\0' || time < 0) {
                         fprintf(stderr, "Встречен неверный формат. Используйте: ./threads -1|-2|-3 [TIME]|-4\n");
                         exit(1);
                     }
                 }
-                printf("%ld", time);
+                third_task();
                 break;
             case '4':
                 if (argc > 2) {
@@ -129,6 +128,39 @@ void second_task() {
     semctl(sem_id, 0, IPC_CREAT);
 }
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void third_task(){
+    pthread_t thread1, thread2;
+    errno = 0;
+    pthread_mutex_init(&mutex, NULL);
+    if (errno){
+        fprintf(stderr, "Невозможно инициаллизировать mutex. Ошибка: %s\n", strerror(errno));
+        exit(1);
+    }
+    pthread_create(&thread1, NULL, task3_thread1, NULL);
+    pthread_create(&thread2, NULL, task3_thread2, NULL);
+    if(errno) {
+        fprintf(stderr, "Невозможно создать потоки. Ошибка: %s\n", strerror(errno));
+        exit(1);
+    }
+    while (true) {
+        pthread_mutex_lock(&mutex);
+        if (errno) {
+            fprintf(stderr, "Ошибка: %s в функции pthread_mutex_lock.\n", strerror(errno));
+            exit(1);
+        }
+        printf("что-то написано\n");
+        usleep(time_threads);
+        pthread_mutex_unlock(&mutex);
+        if (errno) {
+            fprintf(stderr, "Ошибка: %s в функции pthread_mutex_lock.\n", strerror(errno));
+            exit(1);
+        }
+        usleep(time_threads);
+    }
+}
+
 void change_reg() {
     for (int i = 0; i < SIZE; ++i) {
         if (array[i] >= 'A' && array[i] <= 'Z') {
@@ -154,14 +186,14 @@ void *task1_thread1() {
         errno = 0;
         sem_wait(&sem);
         if (errno) {
-            fprintf(stderr, "Невозможно выполнить sem_wait. Ошибка: %s", strerror(errno));
+            fprintf(stderr, "Невозможно выполнить sem_wait. Ошибка: %s\n", strerror(errno));
             exit(1);
         }
         change_reg();
         sleep(1);
         sem_post(&sem);
         if (errno) {
-            fprintf(stderr, "Невозможно выполнить sem_post. Ошибка: %s", strerror(errno));
+            fprintf(stderr, "Невозможно выполнить sem_post. Ошибка: %s\n", strerror(errno));
             exit(1);
         }
         sleep(1);
@@ -172,14 +204,14 @@ void *task1_thread2() {
     while (true) {
         sem_wait(&sem);
         if (errno) {
-            fprintf(stderr, "Невозможно выполнить sem_wait. Ошибка: %s", strerror(errno));
+            fprintf(stderr, "Невозможно выполнить sem_wait. Ошибка: %s\n", strerror(errno));
             exit(1);
         }
         reverse();
         sleep(1);
         sem_post(&sem);
         if (errno) {
-            fprintf(stderr, "Невозможно выполнить sem_post. Ошибка: %s", strerror(errno));
+            fprintf(stderr, "Невозможно выполнить sem_post. Ошибка: %s\n", strerror(errno));
             exit(1);
         }
         sleep(1);
@@ -223,5 +255,43 @@ void *task2_thread2() {
             fprintf(stderr, "Невозможно создать sembuf. Ошибка: %s\n", strerror(errno));
             exit(1);
         }
+    }
+}
+
+void *task3_thread1() {
+    while (true) {
+        usleep(time_threads);
+        if (errno) {
+            fprintf(stderr, "Ошибка: %s в функции pthread_mutex_lock.\n", strerror(errno));
+            exit(1);
+        }
+        pthread_mutex_lock(&mutex);
+        change_reg();
+        usleep(time_threads);
+        pthread_mutex_unlock(&mutex);
+        if (errno) {
+            fprintf(stderr, "Ошибка: %s в функции pthread_mutex_unlock.\n", strerror(errno));
+            exit(1);
+        }
+        usleep(time_threads);
+    }
+}
+
+void *task3_thread2() {
+    while (true) {
+        usleep(time_threads);
+        if (errno) {
+            fprintf(stderr, "Ошибка: %s в функции pthread_mutex_lock.\n", strerror(errno));
+            exit(1);
+        }
+        pthread_mutex_lock(&mutex);
+        reverse();
+        usleep(time_threads);
+        pthread_mutex_unlock(&mutex);
+        if (errno) {
+            fprintf(stderr, "Ошибка: %s в функции pthread_mutex_lock.\n", strerror(errno));
+            exit(1);
+        }
+        usleep(time_threads);
     }
 }
