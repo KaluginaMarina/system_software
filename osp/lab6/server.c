0acb659c1380733c0b97d7d8c6fa6fbbcc51492a
Marina Kalugina <marin.kalugin@gmail.com>
2019-12-06 00:06:53 +0300

    [osp lab6] прослушивание клиентов на сервере

---
+++ osp/lab6/server.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <errno.h>
+#include <zconf.h>
+#include <netinet/in.h>
+
+void check_errno(char *strerr) {
+    if (errno) {
+        fprintf(stderr, "%s. Ошибка: %s\n", strerr, strerror(errno));
+        exit(1);
+    }
+}
+
+void start_server() {
+    int port;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    check_errno("Невозможно создать сокет");
+    // ......sockaddr_in...........
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET; addr.sin_port = htons(port);
+    struct in_addr sin_addr;
+    sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_addr = sin_addr;
+    //.............................
+    bind(sockfd, (struct sockaddr*) &addr, sizeof(struct sockaddr_in));
+    check_errno("Невозможно привязать к сокету локальный адрес");
+    listen(sockfd, 10);
+    check_errno("Невозможно начать прослушивать соединения");
+    while(true) {
+        int client = accept(sockfd, NULL, NULL);
+        check_errno("Ошибка при соединении с клиентом");
+        int frk = fork();
+        check_errno("Невозможно создать подпоток");
+        if (!frk) {
+            printf("Не");
+        } else {
+            printf("Да");
+        }
+    }
+}
+
+int main() {
+    start_server();
+    return 0;
+}
