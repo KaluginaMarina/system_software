a659197db56625b38fa92d03e13a884f49c62c7b
Marina Kalugina <marin.kalugin@gmail.com>
2019-12-06 17:38:14 +0300

    [osp lab6] получение сообщений клиентом

--- osp/lab6/client.c
+++ osp/lab6/client.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <zconf.h>
 
 void check_errno(char *strerr) {
     if (errno) {
@@ -12,7 +13,7 @@ void check_errno(char *strerr) {
 }
 
 
-void getinfo(char* argv[]) {
+void getinfo(int argc, char* argv[]) {
     struct addrinfo addrinfo;
     addrinfo.ai_family = AF_INET;
     addrinfo.ai_socktype = SOCK_STREAM;
@@ -20,11 +21,38 @@ void getinfo(char* argv[]) {
     struct addrinfo *addr;
 
     getaddrinfo(argv[1], argv[2], &addrinfo, &addr);
-    check_errno("Невозможно ")
+    check_errno("Невозможно получить getaddrinfo");
 
-};
+    int sockfd = 0;
+    for (struct addrinfo* a = addr; a != NULL; a = a->ai_next) {
+        if ((sockfd = socket(a->ai_family, a->ai_socktype, a->ai_protocol)) == -1)
+            continue;
+        if (connect(sockfd, a->ai_addr, a->ai_addrlen) == 0)
+            break;
+        sockfd = close(sockfd);
+    }
+    if (sockfd <= 0) {
+        fprintf(stderr, "Невозможно приконнектиться к %s:%s\n", argv[1], argv[2]);
+        exit(1);
+    }
+    freeaddrinfo(addr);
+
+    for (int i = 3; i < argc; ++i) {
+        write(sockfd, argv[i], strlen(argv[i]));
+        //CHK_ERRNO(write(serverfd, CRLF, sizeof(CRLF) - 1));
+    }
+    //CHK_ERRNO(write(serverfd, CRLF, sizeof(CRLF) - 1));
+
+    char *buf = (char*)malloc(2048 * sizeof(char));
+    int bytes_read;
+    while ((bytes_read = read(sockfd, buf, 2048)) > 0) {
+        write(STDOUT_FILENO, buf, bytes_read);
+    }
+
+    close(sockfd);
+}
 
 int main(int argc, char* argv[]) {
-    getinfo();
+    getinfo(argc, argv);
     return 0;
 }
