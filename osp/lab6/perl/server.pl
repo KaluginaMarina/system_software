3935387903991c059c95437e93ffda8d0da35b57
Marina Kalugina <marin.kalugin@gmail.com>
2019-12-07 03:00:50 +0300

    [osp lab6] perl

--- osp/lab6/perl/server.pl
+++ osp/lab6/perl/server.pl
@@ -5,8 +5,7 @@ use warnings qw(FATAL all);
 use threads;
 use IO::Socket::INET;
 
-use constant USAGE => "Используйте ./server port\n";
-my ($port) = shift =~ /(\d+)/ or die USAGE;
+my ($port) = shift =~ /(\d+)/ or die "Используйте ./server port\n";
 
 my $server = IO::Socket::INET->new(
   LocalPort => $port, 
@@ -17,35 +16,31 @@ my $server = IO::Socket::INET->new(
 print "Listening on $port\n";
 
 while (my $client = $server->accept()) {
-  threads->create(\&process_request, $client)->detach();
+  my $pid = fork();
+  die if not defined $pid;
+  if (not $pid) {
+    my $request = "";
+    while (<$client>) {
+      $request .= $_;
+      last if $request =~ /\n\n$/;
+    }
+
+    my @dirs = split /\n/, $request;
+
+    for my $dir (@dirs) {
+      if (opendir my $dirent, $dir) {
+        my @files = readdir $dirent;
+        closedir $dirent;
+
+        print $client "$dir:\n" . join("\n", @files) . "\n\n";
+      }
+      else {
+        print $client "Невозможно открыть $dir: $!\n\n";
+      }
+    }
+
+    close $client;
+  }
 }
 
-sub process_request {
-  my $client = shift;
 
-  my $cltaddr = $client->peerhost;
-  my $cltport = $client->peerport;
-  print "Клиент: $cltaddr:$cltport\n";
-
-  my $request = "";
-  while (<$client>) {
-    $request .= $_;
-    last if $request =~ /\n\n$/;
-  }
-
-  my @dirs = split /\n/, $request;
-
-  for my $dir (@dirs) {
-    if (opendir my $dirent, $dir) {
-      my @files = readdir $dirent;
-      closedir $dirent;
-
-      print $client "$dir:\n" . join("\n", @files) . "\n\n";
-    }
-    else {
-      print $client "Невозможно открыть $dir: $!\n\n";
-    }
-  }
-
-  close $client;
-}
